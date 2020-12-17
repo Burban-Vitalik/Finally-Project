e1ed447104b4973c19c3ce33e9afc3ad5122a3de
Burban-Vitalik <burbanvitalik2002@gmail.com>
2020-12-18 01:16:33 +0200

    HotFIx

--- src/components/Footer/Socials/Socials.jsx
+++ src/components/Footer/Socials/Socials.jsx
@@ -13,17 +13,17 @@ export class Socials extends React.Component {
             <nav className={s.navigation}>
                 <h2>Socials</h2>
                 <ul className={s.listSocials}>
-                    <a href="https://www.youtube.com/premierleague" target="_blank"><YouTubeIcon className={s.youtubeIcon} /> PL on YouTube</a>
-                    <a href="https://www.facebook.com/premierleague" target="_blank"><FacebookIcon className={s.facebookIcon} /> PL on Facebook</a>
-                    <a href="https://www.facebook.com/OfficialFPL/" target="_blank"><FacebookIcon className={s.facebookIcon} /> FPL on Facebook</a>
-                    <a href="https://twitter.com/premierleague" target="_blank"><TwitterIcon className={s.twitterIcon} /> PL on Twitter</a>
-                    <a href="https://twitter.com/OfficialFPL" target="_blank"><TwitterIcon className={s.twitterIcon} /> FPL on Twitter</a>
-                    <a href="https://twitter.com/PLCommunities" target="_blank"><TwitterIcon className={s.twitterIcon} /> PL Communities on Twitter</a>
-                    <a href="https://twitter.com/PLYouth" target="_blank"><TwitterIcon className={s.twitterIcon} /> PL Youth on Twitter </a>
-                    <a href="https://www.instagram.com/premierleague" target="_blank"><InstagramIcon className={s.instagramIcon} /> PL on Instagram</a>
-                    <a href="https://twitter.com/PLinUSA" target="_blank"><TwitterIcon className={s.twitterIcon} /> PL USA on Twitter</a>
-                    <a href="https://www.instagram.com/plinusa/" target="_blank"><InstagramIcon className={s.instagramIcon} /> PL USA on Instagram</a>
-                    <a href="https://open.spotify.com/user/plplaylists" target="_blank"><PlayCircleFilledIcon className={s.spotifyIcon} /> PL Music on Spotify</a>
+                    <a href="https://www.youtube.com/premierleague" rel="noreferrer" target="_blank"><YouTubeIcon className={s.youtubeIcon} /> PL on YouTube</a>
+                    <a href="https://www.facebook.com/premierleague" rel="noreferrer" target="_blank"><FacebookIcon className={s.facebookIcon} /> PL on Facebook</a>
+                    <a href="https://www.facebook.com/OfficialFPL/" rel="noreferrer" target="_blank"><FacebookIcon className={s.facebookIcon} /> FPL on Facebook</a>
+                    <a href="https://twitter.com/premierleague" rel="noreferrer" target="_blank"><TwitterIcon className={s.twitterIcon} /> PL on Twitter</a>
+                    <a href="https://twitter.com/OfficialFPL" rel="noreferrer" target="_blank"><TwitterIcon className={s.twitterIcon} /> FPL on Twitter</a>
+                    <a href="https://twitter.com/PLCommunities" rel="noreferrer" target="_blank"><TwitterIcon className={s.twitterIcon} /> PL Communities on Twitter</a>
+                    <a href="https://twitter.com/PLYouth" rel="noreferrer" target="_blank"><TwitterIcon className={s.twitterIcon} /> PL Youth on Twitter </a>
+                    <a href="https://www.instagram.com/premierleague" rel="noreferrer" target="_blank"><InstagramIcon className={s.instagramIcon} /> PL on Instagram</a>
+                    <a href="https://twitter.com/PLinUSA" rel="noreferrer" target="_blank"><TwitterIcon className={s.twitterIcon} /> PL USA on Twitter</a>
+                    <a href="https://www.instagram.com/plinusa/" rel="noreferrer" target="_blank"><InstagramIcon className={s.instagramIcon} /> PL USA on Instagram</a>
+                    <a href="https://open.spotify.com/user/plplaylists" rel="noreferrer" target="_blank"><PlayCircleFilledIcon className={s.spotifyIcon} /> PL Music on Spotify</a>
                 </ul>
             </nav>
         )
